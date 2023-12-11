ed442335ec9d951270123c3bd7bfe293f5f9209b
Carolina Dutra <dutracarol001@gmail.com>
2023-12-11 10:20:44 -0300

    Realizei a função para marcar o horário do corte

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ function cumprimentarCliente(nome) {
     console.log(`Olá, ${nome}`);
 }
 
+function disponivelAgora() {
+    let horarioEscolhido = prompt('Pode ser às 9h30? (Responda com "sim" ou "não")');
+
+    if (horarioEscolhido.toLowerCase() === 'sim') {
+        return true;
+    } else {
+        let horariosDisponiveis = '13:40, 14:00, 15:30, 16:45, 17:00';
+        console.log('Horários disponíveis: ' + horariosDisponiveis);
+
+        let novoHorario = prompt('Escolha um dos horários disponíveis (' + horariosDisponiveis + ') no formato (hh:mm)');
+
+        while (!horariosDisponiveis.includes(novoHorario)) {
+            console.log('Horário inválido ou não disponível.');
+            novoHorario = prompt('Por favor, escolha um dos horários disponíveis (' + horariosDisponiveis + ') no formato (hh:mm)');
+        }
+
+        return true;
+    }
+}
+
 function iniciarCorte() {
 
 }
