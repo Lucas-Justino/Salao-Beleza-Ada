668c656bb90cac828911363e0df22fe42cdd8682
Lucas Justino <lucas.justino.dev@gmail.com>
2023-12-11 11:25:59 -0300

    Criação do Pagamento e da Exibição do Comprovante

--- script.js
+++ script.js
@@ -43,8 +43,59 @@ function informarValor() {
 
 }
 
-function confirmarValorPago() {
+function pagamentoDinheiro(valor){ //VERIFICACAO DE PAGAMENTO IMPLEMENTADA APENAS PARA DINHEIRO
+    let pagamento = prompt('Informe o valor a ser pago: ')
+    while(pagamento < valor){
+        pagamento = prompt('O valor não cobre o serviço total. Por favor, insira novamente:');
+    }
+    return pagamento - valor;
+}
 
+function pagamentoCartao(valor){
+    alert('Por favor, insira ou aproxime o seu cartão!');
+    alert(`Transação aprovada no valor de R$${valor},00`);
+}
+
+function pagamentoPix(valor){
+    alert('Por favor, leia o QR Code')
+    alert(`Pagamento de R$${valor},00 aprovado`);
+}
+
+function exibirComprovante(formaPagamento, valor, troco){
+    let mensagem = '----------Comprovante de Pagamento----------\n' 
+                 + 'Valor: R$' + valor + ',00.\n'
+                 + 'Forma de Pagamento: ' + formaPagamento + '.\n';
+
+    if(formaPagamento === "Dinheiro")
+        mensagem += 'Troco: R$' + troco + ',00.\n';
+
+    mensagem += 'Obrigado pela preferência. Volte Sempre!\n';
+    alert(mensagem);
+}
+
+function confirmarValorPago(valor) {
+    let opcaoPagamento = prompt('Informe a forma de pagamento:\n 1- Dinheiro\n 2- Cartão\n 3- Pix')
+    let formaPagamento = "";
+    let troco = 0;  
+    switch(opcaoPagamento){
+        case "1":
+            troco = pagamentoDinheiro(valor);
+            formaPagamento = "Dinheiro";
+            break;
+        case "2":
+            pagamentoCartao(valor);
+            formaPagamento = "Cartão";
+            break;
+        case "3":
+            pagamentoPix(valor);
+            formaPagamento = "Pix";
+            break;
+        default:
+            alert("Opção Inexistente! Digite uma opção válida");
+            confirmarValorPago(valor);
+            break;
+    }
+    exibirComprovante(formaPagamento, valor, troco);
 }
 
 
@@ -56,8 +107,8 @@ function salaoDeBeleza() {
     if(disponibilidade) {
         iniciarCorte();
         terminarCorte();
-        informarValor();
-        confirmarValorPago();
+        let valor = informarValor(); //FIZ ESSA ALTERAÇÃO PARA DEVOLVER O VALOR DO CORTE
+        confirmarValorPago(valor);
     } else {
         console.log('Então podemos marcar outra hora!');
     }
